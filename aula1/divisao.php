3a13c9bb9617976dacf63cbcca5ab9aeb69d95b2
laramendes1 <laramendessantos2099@gmail.com>
2025-08-05 23:13:49 -0300

    Criação da calculadora

---
+++ aula1/divisao.php
@@ -0,0 +1,7 @@
+<?php
+    $numero1 = $_POST['numero1'];
+    $numero2 = $_POST['numero2'];
+    $resultado = $numero1 / $numero2;
+
+    echo "$resultado";
+  
---
+++ aula1/subtracao.php
@@ -0,0 +1,6 @@
+<?php
+    $numero1 = $_POST['numero1'];
+    $numero2 = $_POST['numero2'];
+    $resultado = $numero1 - $numero2;
+
+    echo "$resultado";
